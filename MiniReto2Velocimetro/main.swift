15f99a9a20c1ad4999fa6a32bbd1d78f8485f830
Arturo Barreto Villaf√°n <Barreto@arturos-macbook-pro.local>
2015-11-06 16:23:01 -0600

    Initial Commit

---
+++ MiniReto2Velocimetro/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MiniReto2Velocimetro
+//
+//  Created by Arturo Barreto Villafán on 11/6/15.
+//  Copyright © 2015 Arturo Barreto Villafán. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
